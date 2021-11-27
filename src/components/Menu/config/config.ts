e80ae997ba5f7069e2d94075c69d10c2721b5957
Win Nebergall <anatolloflint@gmail.com>
2021-11-27 10:48:09 +0800

    first commit

---
+++ src/components/Menu/config/config.ts
@@ -0,0 +1,26 @@
+import { MenuItemsType, DropdownMenuItemType } from '@pancakeswap/uikit'
+import { ContextApi } from 'contexts/Localization/types'
+import { nftsBaseUrl } from 'views/Nft/market/constants'
+
+export type ConfigMenuItemsType = MenuItemsType & { hideSubNav?: boolean }
+
+const config: (t: ContextApi['t']) => ConfigMenuItemsType[] = (t) => [
+  {
+    label: t('Trade'),
+    icon: 'Swap',
+    href: '/swap',
+    showItemsOnMobile: false,
+    items: [
+      {
+        label: t('Exchange'),
+        href: '/swap',
+      },
+      {
+        label: t('Liquidity'),
+        href: '/liquidity',
+      },
+    ],
+  },
+]
+
+export default config

f0ade1a00581b44844e9dc70f41bbba455a1b9d6
Win Nebergall <anatolloflint@gmail.com>
2021-11-30 11:00:39 +0800

    testnet

--- src/config/index.ts
+++ src/config/index.ts
@@ -13,7 +13,8 @@ export const BSC_BLOCK_TIME = 3
 
 export const BASE_BSC_SCAN_URLS = {
   [ChainId.MAINNET]: 'https://bscscan.com',
-  [ChainId.TESTNET]: 'https://testnet.bscscan.com',
+  // [ChainId.TESTNET]: 'https://testnet.bscscan.com',
+  [ChainId.TESTNET]: 'https://explorer.binance.org/smart-testnet',
 }
 
 export const CHAIN_NAMES = {
